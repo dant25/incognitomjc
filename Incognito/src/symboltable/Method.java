bcde4e385b777d07852832df6a800cfe38322f26
jonasdeyson <jonasdeyson@7bb37152-22ff-11df-a494-6dfd6523cf1c>
2010-04-18 20:31:22 +0000

    Checagem de tipos concluida:     - Visitor TypeCheckingVisitor criado.
    TODO:
        - Corrigir a precedência dos operadores na gramática.
        - Otimizar o percurso da tabela de símbolos e da árvore abstrada onde for possível durante a checagem de tipo.
        - Prosseguir para a fase seguinte.
        
    
    **************************************
    Este commit é fruto da reunião da equipe.

--- Incognito/src/symboltable/Method.java
+++ Incognito/src/symboltable/Method.java
@@ -1,24 +1,32 @@
 package symboltable;
 
+import java.util.ArrayList;
 import java.util.Hashtable;
 
 
 public class Method {
 	private String id;
 	private String return_type;
-			   // id     type
+			         // id     type
 	private Hashtable<String, String> params;
+	private ArrayList<String> param_types;
 	private Hashtable<String, String> locals;
 	
 	public Method(String id_, String return_type_){
 		id = id_;
 		return_type = return_type_;
 		params = new Hashtable<String, String>();
+		param_types = new ArrayList<String>();
 		locals = new Hashtable<String, String>();
 	}
 	
 	public boolean addParam(String id, String type){
-		return params.put(id, type) != null;
+		if(params.put(id, type) == null){
+			param_types.add(type);
+			return true;
+		}
+		
+		return false;
 	}
 	
 	public boolean addLocal(String id, String type){
@@ -29,7 +37,20 @@ public class Method {
 		return id;
 	}
 	
-	public String getType(){
+	public String getReturnType(){
 		return return_type;
 	}
+	
+	public String getLocalVariableType(String id){
+		return locals.get(id);
+	}
+	
+	public String getParameterType(String id){
+		return params.get(id);
+	}
+	
+	public String getParameterType(int order){
+		return param_types.get(order);
+	}
+
 }
